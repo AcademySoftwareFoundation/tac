d781ebfec87461bdd56de39ea391db03c9378ee4
John Mertic <jmertic@linuxfoundation.org>
2022-04-11 16:50:26 -0400

    Update updateprojects.py
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- .github/workflows/updateprojects.py
+++ .github/workflows/updateprojects.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3                                                                                                  
 #                                                                                                                       
-# Copyright this project and it's contributors                                                                          
+# Copyright this project and its contributors                                                                          
 # SPDX-License-Identifier: Apache-2.0                                                                                   
 #                                                                                                                       
 # encoding=utf8
