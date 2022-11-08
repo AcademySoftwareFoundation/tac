7a02f9fcd9943c92953936b1b0bd1275eeba32cf
John Mertic <jmertic@linuxfoundation.org>
2022-11-08 15:40:48 -0500

    Update updateprojects.py
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- .github/workflows/updateprojects.py
+++ .github/workflows/updateprojects.py
@@ -31,6 +31,7 @@ with urllib.request.urlopen(landscapeHostedProjects) as hostedProjectsResponse:
                         'Slug': projectData['id'],
                         'Website': projectData['homepage_url'],
                         'Leads': projectData['extra']['leads'] if 'extra' in projectData and 'leads' in projectData['extra'] else None,
+                        'Documentation': projectData['extra']['documentation'] if 'extra' in projectData and 'documentation' in projectData['extra'] else None,
                         'SBOM': projectData['extra']['SBOM'] if 'extra' in projectData and 'SBOM' in projectData['extra'] else None,
                         'Calendar': projectData['extra']['calendar'] if 'extra' in projectData and 'calendar' in projectData['extra'] else None,
                         'Contribution Guidelines': projectData['extra']['contribution_guidelines'] if 'extra' in projectData and 'contribution_guidelines' in projectData['extra'] else None,
