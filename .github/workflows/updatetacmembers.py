8894b9e4a04c0d83ba6bd7aadb277f9b0127b749
John Mertic <jmertic@linuxfoundation.org>
2023-08-25 08:46:33 -0400

    Add Observers to TAC meeting agenda
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- .github/workflows/updatetacmembers.py
+++ .github/workflows/updatetacmembers.py
@@ -20,17 +20,16 @@ csvRows = []
 with urllib.request.urlopen(committeeURL) as committeeURLResponse:
     committeeURLResponseJSON = json.load(committeeURLResponse)
     for committeeMember in committeeURLResponseJSON['Data']:
-        if committeeMember['VotingStatus'] != 'Observer':
-            print("Processing {} {}...".format(committeeMember['FirstName'],committeeMember['LastName']))
-            csvRows.append({
-                'Full Name': "{} {}".format(committeeMember['FirstName'],committeeMember['LastName']),
-                'Account Name: Account Name': committeeMember['Organization']['Name'] if 'Organization' in committeeMember and 'Name' in committeeMember['Organization'] else None,
-                'Appointed By': committeeMember['AppointedBy'] if 'AppointedBy' in committeeMember else None,
-                'Voting Status': committeeMember['VotingStatus'],
-                'Special Role': committeeMember['Role'],
-                'Title': committeeMember['Title'],
-                'HeadshotURL': committeeMember['LogoURL']
-                })
+        print("Processing {} {}...".format(committeeMember['FirstName'].title(),committeeMember['LastName'].title()))
+        csvRows.append({
+            'Full Name': "{} {}".format(committeeMember['FirstName'].title(),committeeMember['LastName'].title()),
+            'Account Name: Account Name': committeeMember['Organization']['Name'] if 'Organization' in committeeMember and 'Name' in committeeMember['Organization'] else None,
+            'Appointed By': committeeMember['AppointedBy'] if 'AppointedBy' in committeeMember else None,
+            'Voting Status': committeeMember['VotingStatus'],
+            'Special Role': committeeMember['Role'],
+            'Title': committeeMember['Title'],
+            'HeadshotURL': committeeMember['LogoURL']
+            })
 
 with open(tacmembersCsvFile, 'w') as tacmembersCsvFileObject:
     writer = csv.DictWriter(tacmembersCsvFileObject, fieldnames = csvRows[0].keys())
